9d75a6a8713c036e36e79b1c757e76668f1fd353
Elliot Larson <elliot@onehouse.io>
2017-06-09 11:22:18 -0700

    Adding go path

--- zsh/.oh-my-zsh/custom/settings.zsh
+++ zsh/.oh-my-zsh/custom/settings.zsh
@@ -7,6 +7,9 @@ eval "$(pyenv virtualenv-init -)"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$PATH
+
 export PATH="./scripts:$PATH"
 export PATH="~/bin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
