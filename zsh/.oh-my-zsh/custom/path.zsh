fb3046a089398ea9848abd8a209702fb0c44d5f0
Elliot Larson <elliot@onehouse.net>
2017-06-16 10:56:48 -0700

    Some path updates

--- zsh/.oh-my-zsh/custom/path.zsh
+++ zsh/.oh-my-zsh/custom/path.zsh
@@ -1,4 +1,4 @@
 export PATH="./scripts:$PATH"
-export PATH="~/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="$PATH:~/.npm-packages/bin"
+export PATH="$PATH:$HOME/.npm-packages/bin"
