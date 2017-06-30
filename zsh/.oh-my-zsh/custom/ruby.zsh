a28115343ef607db3dcdbd4c0c67d9815dbcd2e3
Elliot Larson <elliot@onehouse.net>
2017-06-29 22:48:14 -0700

    Cleaning up ruby aliases

--- zsh/.oh-my-zsh/custom/ruby.zsh
+++ zsh/.oh-my-zsh/custom/ruby.zsh
@@ -3,25 +3,31 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
 alias b='bundle'
-alias bi='bundle install'
 alias bu='bundle update'
 alias be='bundle exec'
 alias bo='bundle open'
 alias bs='bundle show'
 alias bake='bundle exec rake'
-alias bspec='bundle exec rspec'
-alias bspecs='bundle exec rspec spec'
+spec() {
+  if [ -z "$1" ]; then
+    spec='spec' # run the spec directory
+  else
+    spec="$1"
+  fi
+  bundle exec rspec $spec
+}
+alias r='bundle exec rails'
+alias rac='bundle exec rails console'
+alias ras='bundle exec rails server'
+alias ram='bundle exec rails db:migrate'
+alias rads='bundle exec rails db:setup'
+alias radl='bundle exec rails db:schema:load'
+alias radc='bundle exec rails db:create'
+alias radd='bundle exec rails db:drop'
+alias radt='bundle exec rails db:test:prepare'
 alias raps='rake parallel:spec'
 alias rapp='rake parallel:prepare'
-alias radm='rake db:migrate'
-alias radt='rake db:migrate db:test:prepare'
-alias r='bundle exec rails'
-alias z='zeus'
-alias zake='zeus rake'
-alias zspec='zeus rspec'
-alias zs='zeus start'
-alias zr='zeus rspec'
-alias zc='zeus console'
+alias fors='bundle exec foreman start'
 alias mm='middleman'
 
 last-migration() {
